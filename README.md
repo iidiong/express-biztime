1bee8e5bf6986311ad0bfd8b27814880d336e40a
iidiong <idong30@hotmail.com>
2022-02-13 14:41:31 -0500

    remove coverage report

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Express-Biztime
+
+## Seed test db
+    - change to biztime_test in data.sql
+    - psql biztime_test
+    - \i data.sql
+
+## Seed Production db
+    - change to biztime in data.sql
+    - psql biztime
+    - \i data.sql
+
+## Run Test
+    - npm run test
+
+## Run app in normal mode
+    - npm run start
+  
+  
+## Run app in debug mode
+    - npm run dev
+  
--- app.test.js
+++ app.test.js
@@ -3,17 +3,19 @@ const request = require("supertest");
 const app = require('./app');
 const db = require('./db');
 
+let invoiceId;
 
-describe("Test companies", () => {
+describe("Test companies and invoice routes", () => {
+    
     test("should return all companies", async () => {
         const resp = await request(app).get("/companies");
         expect(resp.statusCode).toBe(200);
     });
-    xtest("should return company by code", async () => {
+    test("should return company by code", async () => {
         const resp = await request(app).get("/companies/apple");
         expect(resp.statusCode).toBe(200);
     });
-    xtest("should return 404 for invalid company code", async () => {
+    test("should return 404 for invalid company code", async () => {
         const resp = await request(app).get("/companies/apple1");
         expect(resp.statusCode).toBe(404);
     });
@@ -47,39 +49,42 @@ describe("Test companies", () => {
             }
         })
     });
-    test("should delete company from db", async () => {
-        const resp = await request(app).delete("/companies/Citi");
-        expect(resp.statusCode).toBe(200);
-    });
-});
-describe("Test invoices", () => {
-    xtest("should return all invoices", async () => {
+
+    test("should return all invoices", async () => {
         const resp = await request(app).get("/invoices");
         expect(resp.statusCode).toBe(200);
     });
-    xtest("should return invoice by code", async () => {
-        const resp = await request(app).get("/invoices/1");
-        expect(resp.statusCode).toBe(200);
-    });
-    xtest("should return 404 for invalid invoices code", async () => {
-        const resp = await request(app).get("/invoices/100");
-        expect(resp.statusCode).toBe(404);
-    });
-    xtest("should create new invoice", async () => {
+    test("should create new invoice", async () => {
         const resp = await request(app).post("/invoices").send({
             "comp_code": "Citi",
             "amt": "500"
         });
+        let result = await request(app).get("/invoices")
+        invoiceId = await result.body.invoices[result.body.invoices.length - 1].id;
         expect(resp.statusCode).toBe(201);
     });
-    xtest("should update invoice 'name' and 'description'", async () => {
+    test("should return invoice by id", async () => {
+        const resp = await request(app).get(`/invoices/${invoiceId}`);
+        expect(resp.statusCode).toBe(200);
+    });
+    test("should return 404 for invalid invoices code", async () => {
+        const resp = await request(app).get("/invoices/100");
+        expect(resp.statusCode).toBe(404);
+    });
+   
+    test("should update invoice 'name' and 'description'", async () => {
         const resp = await request(app).put("/invoices/1").send({
             "amt": "790"
         });
         expect(resp.statusCode).toBe(200);
     });
+    test("should delete invoice from db", async () => {
+        
+        const resp = await request(app).delete(`/invoices/${invoiceId}`);
+        expect(resp.statusCode).toBe(200);
+    });
     test("should delete company from db", async () => {
-        const resp = await request(app).delete("/invoices/Citi");
+        const resp = await request(app).delete("/companies/Citi");
         expect(resp.statusCode).toBe(200);
     });
 });
